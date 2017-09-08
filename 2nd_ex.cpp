1e29ef2e57f75324a416d4297437b53144b521f9
ortonormal2007 <yaroslav_kudlaev@mail.ru>
2017-09-08 14:17:28 +0700

    Update 2nd_ex.cpp

--- 2nd_ex.cpp
+++ 2nd_ex.cpp
@@ -7,7 +7,8 @@ int main(){
 
 	float sum_bl = 0;
 	float sum_lb = 0;
-	float sum_lb_sep = 0;
+	float sum_lb_pos = 0;
+	float sum_lb_neg = 0;
 	float sum_bl_sep = 0;
 
 	for (int i = 10000; i > 0; i--){
@@ -19,10 +20,10 @@ int main(){
 	}
 
 	for (int i = 2; i <= 10000; i = i + 2){
-		sum_lb_sep += (pow(-1, i)) / i;
+		sum_lb_pos += (pow(-1, i)) / i;
 	}
 	for (int i = 1; i < 10000; i = i + 2){
-		sum_lb_sep += (pow(-1, i)) / (i);
+		sum_lb_neg += (pow(-1, i)) / (i);
 	}
 
 	for (int i = 10000; i > 1; i = i - 2){
@@ -30,13 +31,8 @@ int main(){
 		sum_bl_sep += (pow(-1, i)) / i;
 	}
 
-	//printf("little to big: ");
 	printf("little to big: %f\n", sum_bl);
-
-	//printf("big to little: ");
 	printf("big to little: %f\n", sum_lb);
-
-	
 	printf("little to big separatly: %f\n", sum_lb_sep);
 	printf("big to little separatly: %f\n", sum_bl_sep);
 
