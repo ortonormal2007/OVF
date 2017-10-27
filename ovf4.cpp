8f0f026e0b95ae744838bc58c42afac2844baf88
ortonormal2007 <yaroslav_kudlaev@mail.ru>
2017-10-27 19:25:07 +0700

    Update ovf4.cpp

--- ovf4.cpp
+++ ovf4.cpp
@@ -2,8 +2,8 @@
 #include <cmath>
 #include <fstream>
 
-#define REF1 1.57079632679
-#define REF2 1.29587400873
+#define REF1 1.57079632679*0.5
+#define REF2 2.97314800644958
 
 using namespace std;
 
@@ -26,88 +26,104 @@ double paral(double(*func)(double), double a, double b, int num){
 	return I;
 }
 
-double simps(double(*func)(double), double a, double b, int num){
-
+double simps(double(*func)(double), double a, double b, double num){
 	double I;
 	I = 0;
+	double ainit = a;
 	double dx = (b - a) / num;
+	I += (dx / 3.)*func(a);
+	
+	int n = 1;
+	
+	a = ainit + dx;
+
+	
+
 	while (a < b){
-		I += (dx / 6.)*(func(a) + (4 * func((a + a + dx) / 2.)) + func(a + dx));
-		a += dx;
+		I += 4 * (dx / 3.)*func(a);
+		n += 2;
+		a = ainit + n*dx;
+
 	}
+	a = ainit + 2 * dx;
+	n = 2;
+	while (a < b){
+		I += 2 * (dx / 3.)*func(a);
+		n += 2;
+		a = ainit + n*dx;
+
+	}
+
+	I += (dx / 3.)*func(b);
+	/*while (a < b){
+		I += (dx / 6.)*(func(a) + (4. * func(a + (dx / 2.))) + func(a + dx));
+		a += dx;
+	}*/
 	return I;
 }
 
 int main(){
-	int nummax = 2048;
-	int num = 1;
-	double res;
-	double res1;
+	double nummax = 4096;
+	double num = 2;
 	double ratio;
-	ofstream lalala("ratio(2_sim).txt");
-	while (num < nummax){
-		res = fabs(REF2 - simps(func2, 0, 1, num));
-		res1 = fabs(REF2 - simps(func2, 0, 1, num * 2));
-		ratio = res / res1;
+	ofstream lalala("simps1.txt");
+	while (num <= nummax){
+		ratio = abs((REF2 - simps(func2, 1, 2, num))/(REF2- simps(func2, 1, 2, num * 2)));
 		lalala << num << "\t" << ratio << endl;
 		num *= 2;
 	}
 
-
-	//double * parr1 = prestest1(func1, -1, 1, 2048);
-	//double * parr2 = prestest2(func2, 0, 1, 2048);
 	
-	/*
-	double a, b;
+
+	/*double a, b;
 	a = 0;
 	b = 0;
 
 	cin >> a;
 	cin >> b;
 
-	double I_1P4 = paral(func1, a, b, 4);
-	printf("%f\n", I_1P4);
+	double i_1p4 = paral(func1, a, b, 4);
+	printf("%f\n", i_1p4);
 
-	double I_1P8 = paral(func1, a, b, 8);
-	printf("%f\n", I_1P8);
+	double i_1p8 = paral(func1, a, b, 8);
+	printf("%f\n", i_1p8);
 
-	double I_1P16 = paral(func1, a, b, 16);
-	printf("%f\n", I_1P16);
+	double i_1p16 = paral(func1, a, b, 16);
+	printf("%f\n", i_1p16);
 
-	printf("___________\n", I_1P16);
+	printf("___________\n", i_1p16);
 
-	double I_1S4 = simps(func1, a, b, 4);
-	printf("%f\n", I_1S4);
+	double i_1s4 = simps(func1, a, b, 4);
+	printf("%f\n", i_1s4);
 
-	double I_1S8 = simps(func1, a, b, 8);
-	printf("%f\n", I_1S4);
+	double i_1s8 = simps(func1, a, b, 8);
+	printf("%f\n", i_1s4);
 
-	double I_1S16 = simps(func1, a, b, 16);
-	printf("%f\n", I_1S4);
+	double i_1s16 = simps(func1, a, b, 16);
+	printf("%f\n", i_1s4);
 
 	cin >> a;
 	cin >> b;
 
-	double I_2P4 = paral(func2, a, b, 4);
-	printf("%f\n", I_2P4);
+	double i_2p4 = paral(func2, a, b, 4);
+	printf("%f\n", i_2p4);
 
-	double I_2P8 = paral(func2, a, b, 8);
-	printf("%f\n", I_2P8);
+	double i_2p8 = paral(func2, a, b, 8);
+	printf("%f\n", i_2p8);
 
-	double I_2P16 = paral(func2, a, b, 16);
-	printf("%f\n", I_2P16);
+	double i_2p16 = paral(func2, a, b, 16);
+	printf("%f\n", i_2p16);
 
-	printf("___________\n", I_1P16);
+	printf("___________\n", i_1p16);
 
-	double I_2S4 = simps(func2, a, b, 4);
-	printf("%f\n", I_2S4);
+	double i_2s4 = simps(func2, a, b, 4);
+	printf("%f\n", i_2s4);
 
-	double I_2S8 = simps(func2, a, b, 8);
-	printf("%f\n", I_2S8);
+	double i_2s8 = simps(func2, a, b, 8);
+	printf("%f\n", i_2s8);
 
-	double I_2S16 = simps(func2, a, b, 16);
-	printf("%f\n", I_2S16);
-	*/
-
+	double i_2s16 = simps(func2, a, b, 16);
+	printf("%f\n", i_2s16);
+*/
 	return 0;
 }
