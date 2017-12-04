257a70f2b116e631549c8577ef029c1b9ff841ae
ortonormal2007 <yaroslav_kudlaev@mail.ru>
2017-12-04 20:37:42 +0700

    iteration process was improved

--- ovf9_stiff_equ_iter.cpp
+++ ovf9_stiff_equ_iter.cpp
@@ -4,69 +4,72 @@
 using namespace std;
 
 double right1(double u, double v){
-	return 998 * u + 1998 * v;
+    return 998 * u + 1998 * v;
 }
 
 double right2(double u, double v){
-	return -999 * u - 1999 * v;
+    return -999 * u - 1999 * v;
 }
 
 void substanded(double x, double y, double h, double tfinal){
-	FILE * phase_plane;
-	phase_plane = fopen("phase plane.txt", "w");
-	int n = 1;
-	double x_next = 0;
-	double y_next = 0;
-	double t = 0;
+    FILE * phase_plane;
+    phase_plane = fopen("pp.txt", "w");
+    int n = 1;
+    double x_next = 0;
+    double y_next = 0;
+    double t = 0;
 
-	fprintf(phase_plane, "%g\t%.10f\t%.10f\n", t, x, y);
-	
-	double t_cur = t + h;
-	double x0 = 0;
-	double y0 = 0;
+    fprintf(phase_plane, "%g\t%.10f\t%.10f\n", t, x, y);
 
-	while (t_cur <= tfinal){
+    double t_cur = t + h;
+    double x0 = 0;
+    double y0 = 0;
 
-		//FIRST ITERATION FROM VALUE OF PREV POINT
-		x_next = x - ((x - right1(x, y) - x)/1000);
-		y_next = y - ((y - right2(x, y) - y)/1000);
+    while (t_cur <= tfinal){
 
-		//SIMPLE ITERATIONS PROCESS
-		while ((abs(x_next - x0) > 0.001) || (abs(y_next - y0) > 0.001)){
-			x0 = x_next;
-			y0 = y_next;
-			x_next = x0 - ((x0 - right1(x0, y0) - x)/1000);
-			y_next = y0 - ((y0 - right2(x0, y0) - y)/1000);
+        //FIRST ITERATION FROM VALUE OF PREV POINT
+        x_next = x - ((x - h * right1(x, y) - x)/1.05);
+        y_next = y - ((y - h * right2(x, y) - y)/1.05);
 
-		}
+        //SIMPLE ITERATIONS PROCESS
+        while ((abs(x_next - x0) > 0.0001) || (abs(y_next - y0) > 0.0001)){
+            x0 = x_next;
+            y0 = y_next;
+            x_next = x0 - ((x0 - h * right1(x0, y0) - x)/1.05);
+            y_next = y0 - ((y0 - h * right2(x0, y0) - y)/1.05);
 
-		//NEXT STEP OF GRID
-		x = x_next;
-		y = y_next;
-		fprintf(phase_plane, "%g\t%.10f\t%.10f\n", t_cur, x, y);
-		t_cur = t + h*n;
-		n++;
-	}
+        }
 
-	fclose(phase_plane);
+        //NEXT STEP OF GRID
+        x = x_next;
+        y = y_next;
+
+        fprintf(phase_plane, "%g\t%.10f\t%.10f\n", t_cur, x, y);
+        n++;
+        t_cur = t + h*n;
+
+    }
+
+    fclose(phase_plane);
 }
 
 int main()
 {
-	double xinit = 0;
-	double yinit = 0;
-	double tfinal = 0;
+    double xinit = 0;
+    double yinit = 0;
+    double tfinal = 0;
 
-	cout << "Enter x(t_0)" << endl;
-	cin >> xinit;
-	cout << "Enter y(t_0)" << endl;
-	cin >> yinit;
-	cout << "Enter t_final" << endl;
-	cin >> tfinal;
+    cout << "Enter x(t_0)" << endl;
+    cin >> xinit;
+    cout << "Enter y(t_0)" << endl;
+    cin >> yinit;
+    cout << "Enter t_final" << endl;
+    cin >> tfinal;
 
-	substanded(xinit, yinit, 0.001, tfinal);
-	cout << "program has been done! press any key..." << endl;
+    substanded(xinit, yinit, 0.00001, tfinal);
+    cout << "program has been done! press any key..." << endl;
 
-	return 0;
+    return 0;
 }
+
 
