2f7fde8801d9d06746ba6fd962804b62c57e25fb
ortonormal2007 <yaroslav_kudlaev@mail.ru>
2017-12-21 23:08:29 +0700

    Update ovf12.cpp

--- ovf12.cpp
+++ ovf12.cpp
@@ -6,6 +6,7 @@
 
 
 #define n 4096
+
 //#define pi 3.141592653589
 
 using namespace std;
@@ -19,7 +20,6 @@ void fourier (fftw_complex * in, fftw_complex * out){
     fftw_execute(p);
     fftw_destroy_plan(p);
 }
-
 int main(){
     no_wisdom ();
     ofstream four("four.txt");
@@ -35,27 +35,20 @@ int main(){
 
     for (int i = 0; i < n; i++)
         {
-            orig[i][0] = sin(1000*(a + i * t))/* * 0.5*(1 - cos(2 * M_PI*(i) / n))*/;
+            orig[i][0] = sin(1024*(a + i * t))/* * 0.5*(1 - cos(2 * M_PI*(i) / n))*/;
             orig[i][1] = 0;
-            func << (a + i * t) << "\t" << orig[i][0]<< "\t" << orig[i][1] << endl;
-        }
-
-    for (int i = 0; i < n; i++)
-        {
-            orig[i][0] /= n;
-            orig[i][1] /= n;
             func << (a + i * t) << "\t" << orig[i][0]<< "\t" << orig[i][1] << endl;
         }
 
     fourier(orig, trans);
 
-    for (int i = 0; i < n; i++)
-        {
-            //trans[i][0] = sin(2 * M_PI * (a + i*t));
-           // trans[i][1] = 0;
+    for (int i = 0; i < n; i++){
+        trans[i][0] /= n;
+        trans[i][1] /= n;
         four << (a * 2 * M_PI / (b - a) + t*i * 2 * M_PI / (b - a)) << "\t" << trans[i][0]<< "\t" << trans[i][1] << endl;
-        W << a * 2 * M_PI / (b - a) + t*i * 2 * M_PI / (b - a)  <<  "\t" << pow(trans[i][0],2) + pow(trans[i][1],2) << endl;
+        W << a * 2 * M_PI / (b - a) + i * 2 * M_PI / (b - a)  <<  "\t" << pow(trans[i][0],2) + pow(trans[i][1],2) << endl;
     }
     cout << "Done!" << endl;
     return 0;
 }
+
